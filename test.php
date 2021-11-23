08a52e49871c223ef77e358b529c7f52e79493e0
cameleon <45.worker@protonmail.com>
2021-11-23 16:47:51 +0100

    envoie

--- test.php
+++ test.php
@@ -1,6 +1,9 @@
 <?php
 try {
 require_once('connect.php');
+$connexion=new PDO('mysql:host='.SERVEUR.';dbname='.BDD,USER,PASSWORD);
+$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$connexion->query('SET NAMES UTF8');
 $requete="select nom,prenom from auteur'' ;
 $resultat=$connexion->query($requete);
 $resultat->setFetchMode(PDO::FETCH_OBJ);
