e6dfa27c104a286e7183311a7028d7559685601e
quintlg <quint28140@gmail.com>
2021-11-24 10:05:05 +0100

    Themeplate des fichiers

---
+++ vue/gabarit.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <title>Ma page</title>
+      <meta charset="utf-8">
+	    <link rel="stylesheet"  href="vue/Style/style.css" />
+    </head>
+  	<body>
+
+
+
+      <?php
+        echo $contenu;
+      ?>
+  	</body>
+ </html>
