8d453f36f910712b34413d9b288aba783d19f7b5
quintlg <quint28140@gmail.com>
2021-12-11 16:36:52 +0100

    modif bdd rang int->text (clarté)

--- vue/gabaritdirec.php
+++ vue/gabaritdirec.php
@@ -6,9 +6,26 @@
 	    <link rel="stylesheet"  href="vue/Style/style.css" />
     </head>
   	<body>
-
-          <p>directeur</p>
-
+      <div class="main">
+        <form id="agentedit" action="site.php" method='post'>
+          <fieldset>
+            <legend>Ajout d'Employée</legend>
+            <p>
+              Nom : <input type="text" name="nomcompte"/>
+            </p>
+            <p>
+              Mot de passe : <input type="password" name="mdp"/>
+            </p>
+            <p>
+              Rang : <select name="Rang">
+                <option value="Agent">Agent</option>
+                <option value="Conseiller">Conseiller</option>
+            </p>
+            <p>
+              <input type="submit" value="Ajouter" name="editclient"/>
+            </p>
+          </fieldset>
+        </form>
       <?php
         echo $contenu;
       ?>
