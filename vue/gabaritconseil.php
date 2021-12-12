41cc975ba1fa276403a082ad1f2eb9437c933fea
QGA20113 <quentin.gavois56@gmail.com>
2021-12-12 19:27:34 +0100

    ajout de label

--- vue/gabaritconseil.php
+++ vue/gabaritconseil.php
@@ -10,9 +10,9 @@
       <fieldset>
         <legend>Consulter un planning</legend>
         <form method="post" action="site.php">
-          <label for="edt_cons">Consulter l'EDT d'un conseilller<input type="radio" id="edt_cons" name="selection" value="edt_conseiller"/>
-          <input type="radio" name="selection" value="edt_jour" required/>
-          <input type='submit' name='validation' value="Valider choix" required/>
+          <label for="edt_cons">Consulter l'EDT d'un conseilller</label><input type="radio" id="edt_cons" name="selection" value="edt_conseiller" required/>
+          <label for="edt_jour">Consulter l'EDT d'un jour</label><input type="radio" name="selection" value="edt_jour" required/>
+          <input type='submit' name='validation' value="Valider choix" />
         </form>
       </fieldset>
   <?php echo $contenuselect ?>
