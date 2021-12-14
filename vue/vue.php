eef392c004555668cce451a65f0b445f7c7ec643
cameleon <45.worker@protonmail.com>
2021-12-14 11:53:24 +0100

    ajout select vue

--- vue/vue.php
+++ vue/vue.php
@@ -278,6 +278,27 @@ function statcontrat($cmptcontrat){
     $contenu.="</fieldset>";
     require_once('gabaritdirec.php');
 }
+function rdvpris($rdv_reserver){
+    $contenu="<fieldset>";
+    $contenu.='<legend class="statctr">stat contrat</legend>';
+    $contenu.='<p>'.$rdv_reserver.'</p>';
+    $contenu.="</fieldset>";
+    require_once('gabaritdirec.php');
+}
+function tot_cli($total_client){
+    $contenu="<fieldset>";
+    $contenu.='<legend class="statctr">stat contrat</legend>';
+    $contenu.='<p>'.$total_client.'</p>';
+    $contenu.="</fieldset>";
+    require_once('gabaritdirec.php');
+}
+function tot_solde($totalsolde){
+    $contenu="<fieldset>";
+    $contenu.='<legend class="statctr">stat contrat</legend>';
+    $contenu.='<p>'.$totalsolde.'</p>';
+    $contenu.="</fieldset>";
+    require_once('gabaritdirec.php');
+}
 function affichageEDT($rdvs){
   $contenu='<form action="site.php" method="post">';
   $contenu.="<fieldset>";
